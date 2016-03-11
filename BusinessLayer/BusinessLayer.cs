1b171f32fd3fcafe4d64a5bdf79018f04af24f56
Nicholas <ngrant40@gmail.com>
2016-03-11 13:41:55 -0800

    Added update student and studentsubmenu

--- BusinessLayer/BusinessLayer.cs
+++ BusinessLayer/BusinessLayer.cs
@@ -5,93 +5,107 @@ using System.Linq.Expressions;
 using System.Linq;
 
 namespace BusinessLayer {
-    public interface IBusinessLayer {
-        IList<Standard> GetAllStandards();
-        IQueryable<Standard> SearchForStandard(
-           Expression<Func<Standard, bool>> predicate);
-        Standard GetStandardByID(int id);
-        void AddStandard(Standard standard);
-        void UpdateStandard(Standard standard);
-        void RemoveStandard(Standard standard);
-        Standard GetSingle(Func<Standard, bool> where,
-           params Expression<Func<Standard, object>>[] navigationProperties);
-        IList<Student> GetAllStudents();
-        IQueryable<Student> SearchForStudent(Expression<Func<Student,
-           bool>> predicate);
-        Student GetStudentByID(int id);
-        void AddStudent(Student student);
-        void UpdateStudent(Student student);
-        void RemoveStudent(Student student);
-        Student GetSingle(Func<Student, bool> where,
-           params Expression<Func<Student, object>>[] navigationProperties);
-    }
-    public class BusinessLayer : IBusinessLayer {
-        private readonly IStandardRepository _standardRepository;
-        private readonly IStudentRepository _studentRepository;
+	public interface IBusinessLayer {
+		IList<Standard> GetAllStandards();
+		IQueryable<Standard> SearchForStandard(
+			Expression<Func<Standard, bool>> predicate);
+		Standard GetStandardByName(string standardName);
+		Standard GetStandardByID(int id);
+		void AddStandard(Standard standard);
+		void UpdateStandard(Standard standard);
+		void RemoveStandard(Standard standard);
+		Standard GetSingle(Func<Standard, bool> where,
+			params Expression<Func<Standard, object>>[] navigationProperties);
+		IList<Student> GetAllStudents();
+		IQueryable<Student> SearchForStudent(Expression<Func<Student,
+			bool>> predicate);
+		Student GetStudentByName(string standardName);
+		Student GetStudentByID(int id);
+		void AddStudent(Student student);
+		void UpdateStudent(Student student);
+		void RemoveStudent(Student student);
+		Student GetSingle(Func<Student, bool> where,
+			params Expression<Func<Student, object>>[] navigationProperties);
+	}
+	public class BusinessLayer : IBusinessLayer {
+		private readonly IStandardRepository _standardRepository;
+		private readonly IStudentRepository _studentRepository;
 
-        public BusinessLayer() {
-            _standardRepository = new StandardRepository();
-            _studentRepository = new StudentRepository();
-        }
+		public BusinessLayer() {
+			_standardRepository = new StandardRepository();
+			_studentRepository = new StudentRepository();
+		}
 
-        public void AddStandard(Standard standard) {
-            _standardRepository.Insert(standard);
-        }
+		public void AddStandard(Standard standard) {
+			_standardRepository.Insert(standard);
+		}
 
-        public void AddStudent(Student student) {
-            _studentRepository.Insert(student);
-        }
+		public void AddStudent(Student student) {
+			_studentRepository.Insert(student);
+		}
 
-        public IList<Standard> GetAllStandards() {
-            return (IList<Standard>)_standardRepository.GetAll();
-        }
+		public IList<Standard> GetAllStandards() {
+			return (IList<Standard>)_standardRepository.GetAll();
+		}
 
-        public Standard GetSingle(Func<Standard, bool> where,
-           params Expression<Func<Standard, object>>[] navigationProperties) {
-           return _standardRepository.GetSingle(where, navigationProperties);
-        }
+		public Standard GetSingle(Func<Standard, bool> where,
+			params Expression<Func<Standard, object>>[] navigationProperties) {
+			return _standardRepository.GetSingle(where, navigationProperties);
+		}
 
-        public Student GetSingle(Func<Student, bool> where,
-           params Expression<Func<Student, object>>[] navigationProperties) {
-           return _studentRepository.GetSingle(where, navigationProperties);
-        }
+		public Student GetSingle(Func<Student, bool> where,
+			params Expression<Func<Student, object>>[] navigationProperties) {
+			return _studentRepository.GetSingle(where, navigationProperties);
+		}
 
-        public IList<Student> GetAllStudents() {
-            return (IList<Student>)_studentRepository.GetAll();
-        }
+		public IList<Student> GetAllStudents() {
+			return (IList<Student>)_studentRepository.GetAll();
+		}
 
-        public Standard GetStandardByID(int id) {
-            return _standardRepository.GetById(id);
-        }
+		public Standard GetStandardByName(string standardName) {
+			return _standardRepository.GetSingle(
+				d => d.StandardName.Equals(standardName),
+				d => d.Students);
+		}
 
-        public Student GetStudentByID(int id) {
-            return _studentRepository.GetById(id);
-        }
+		public Student GetStudentByName(string studentName) {
+			return _studentRepository.GetSingle(
+				d => d.StudentName.Equals(studentName),
+				d => d.Courses);
+		}
 
-        public void RemoveStandard(Standard standard) {
-            _standardRepository.Delete(standard);
-        }
+		public Standard GetStandardByID(int id) {
+			return _standardRepository.GetById(id);
+		}
 
-        public void RemoveStudent(Student student) {
-            _studentRepository.Delete(student);
-        }
+		public Student GetStudentByID(int id) {
+			return _studentRepository.GetById(id);
+		}
 
-        public IQueryable<Standard> SearchForStandard(Expression<Func<Standard,
-           bool>> predicate) {
-            return _standardRepository.SearchFor(predicate);
-        }
+		public void RemoveStandard(Standard standard) {
+			_standardRepository.Delete(standard);
+		}
 
-        public IQueryable<Student> SearchForStudent(Expression<Func<Student,
-           bool>> predicate) {
-            return _studentRepository.SearchFor(predicate);
-        }
+		public void RemoveStudent(Student student) {
+			_studentRepository.Delete(student);
+		}
 
-        public void UpdateStandard(Standard standard) {
-            _standardRepository.Update(standard);
-        }
+		public IQueryable<Standard> SearchForStandard(Expression<Func<Standard,
+			bool>> predicate) {
+			return _standardRepository.SearchFor(predicate);
+		}
 
-        public void UpdateStudent(Student student) {
-            _studentRepository.Update(student);
-        }
-    }
+		public IQueryable<Student> SearchForStudent(Expression<Func<Student,
+			bool>> predicate) {
+			return _studentRepository.SearchFor(predicate);
+		}
+
+		public void UpdateStandard(Standard standard) {
+			_standardRepository.Update(standard);
+		}
+
+		public void UpdateStudent(Student student) {
+			_studentRepository.Update(student);
+		}
+	}
 }
--- BusinessLayer/BusinessLayerTester.cs
+++ BusinessLayer/BusinessLayerTester.cs
@@ -1,34 +1,37 @@
 ﻿using System;
 
 namespace BusinessLayer {
-   class BusinessLayerTester {
+	class BusinessLayerTester {
 
-      private static BusinessLayer businessL;
+		private static BusinessLayer businessL;
 
-      static void Main(string[] args) {
-         businessL = new BusinessLayer();
-         int menuOptions = 1;
-         int menuChoice;
-         bool exitProgram = false;
+		static void Main(string[] args) {
+			businessL = new BusinessLayer();
+			int menuOptions = 1;
+			int menuChoice;
+			bool exitProgram = false;
 
-         do {
-            MenuOptions(ref menuOptions);
-            menuChoice = UserMenuChoice(menuOptions);
-            switch (menuOptions) {
-               case 1:
-                  MainMenu(menuChoice, ref menuOptions, ref exitProgram);
-                  break;
-               case 2:
-                  StandardMenu(menuChoice, ref menuOptions);
-                  break;
-               case 3:
-                  StudentMenu(menuChoice, ref menuOptions);
-                  break;
-            }
-         } while (!exitProgram);
+			do {
+				MenuOptions(ref menuOptions);
+				menuChoice = UserMenuChoice(menuOptions);
+				switch (menuOptions) {
+					case 1:
+						MainMenu(menuChoice, ref menuOptions, ref exitProgram);
+						break;
+					case 2:
+						StandardMenu(menuChoice, ref menuOptions);
+						break;
+					case 3:
+						StudentMenu(menuChoice, ref menuOptions);
+						break;
+					case 4:
+						StudentSubMenu(menuChoice, ref menuOptions);
+						break;
+				}
+			} while (!exitProgram);
 
 
-         /* HardCoded
+			/* HardCoded
          businessL.GetStudentByID(2).StudentName = "BlAdam";
          businessL.UpdateStudent(businessL.GetStudentByID(2));
 
@@ -78,181 +81,231 @@ namespace BusinessLayer {
          foreach (var element in standards) {
              Console.WriteLine(element.StandardId + ": " + element.StandardName);
          }*/
-      }
+		}
 
-      public static void MenuOptions(ref int menu) {
-         switch (menu) {
-            case 1:
-               Console.WriteLine("Menu:");
-               Console.WriteLine("1. Standard Tables");
-               Console.WriteLine("2. Student Tables");
-               Console.WriteLine("3. Exit Program");
-               break;
-            case 2:
-               Console.WriteLine("Standard Menu");
-               Console.WriteLine("1. Display all current Standards");
-               Console.WriteLine("2. Create new Standard");
-               Console.WriteLine("3. Update Standard");
-               Console.WriteLine("4. Delete Standard");
-               Console.WriteLine("5. Display all Standards by an ID");
-               Console.WriteLine("6. Return to Main Menu");
-               break;
-            case 3:
-               Console.WriteLine("Student Menu");
-               Console.WriteLine("1. Display all current Students");
-               Console.WriteLine("2. Create new Student");
-               Console.WriteLine("3. Update Student");
-               Console.WriteLine("4. Delete Student");
-               Console.WriteLine("5. Return to Main Menu");
-               break;
-            default:
-               menu = 1;
-               break;
-         }
-      }
+		public static void MenuOptions(ref int menu) {
+			switch (menu) {
+				case 1:
+					Console.WriteLine("Menu:");
+					Console.WriteLine("1. Standard Tables");
+					Console.WriteLine("2. Student Tables");
+					Console.WriteLine("3. Exit Program");
+					break;
+				case 2:
+					Console.WriteLine("Standard Menu");
+					Console.WriteLine("1. Display all current Standards");
+					Console.WriteLine("2. Create new Standard");
+					Console.WriteLine("3. Update Standard");
+					Console.WriteLine("4. Delete Standard");
+					Console.WriteLine("5. Display all Standards by an ID");
+					Console.WriteLine("6. Return to Main Menu");
+					break;
+				case 3:
+					Console.WriteLine("Student Menu");
+					Console.WriteLine("1. Display all current Students");
+					Console.WriteLine("2. Create new Student");
+					Console.WriteLine("3. Update Student");
+					Console.WriteLine("4. Delete Student");
+					Console.WriteLine("5. Return to Main Menu");
+					break;
+				case 4:
+					Console.WriteLine("Student Update Menu");
+					Console.WriteLine("1. Search by ID");
+					Console.WriteLine("2. Search by name");
+					break;
+				default:
+					menu = 1;
+					break;
+			}
+		}
 
-      public static int UserMenuChoice(int menu) {
-         int userIntChoice;
-         while (true) {
-            string userStringChoice = Console.ReadLine();
-            if (Int32.TryParse(userStringChoice, out userIntChoice)) {
-               if (IsValidMenuChoice(userIntChoice, menu)) {
-                  return userIntChoice;
-               } else {
-                  Console.WriteLine("\nInvalid menu option entered.");
-               }
-            } else {
-               Console.WriteLine("Invalid input entered.");
-            }
-         }
-      }
+		public static int UserMenuChoice(int menu) {
+			int userIntChoice;
+			while (true) {
+				string userStringChoice = Console.ReadLine();
+				if (Int32.TryParse(userStringChoice, out userIntChoice)) {
+					if (IsValidMenuChoice(userIntChoice, menu)) {
+						return userIntChoice;
+					}
+					else {
+						Console.WriteLine("\nInvalid menu option entered.");
+					}
+				}
+				else {
+					Console.WriteLine("Invalid input entered.");
+				}
+			}
+		}
 
-      public static bool IsValidMenuChoice(int userChoice, int menu) {
-         switch (menu) {
-            case 1:
-               return (userChoice > 0 && userChoice <= 3) ? true : false;
-            case 2:
-               return (userChoice > 0 && userChoice <= 6) ? true : false;
-            case 3:
-               return (userChoice > 0 && userChoice <= 5) ? true : false;
-            default:
-               return false;
-         }
-      }
+		public static bool IsValidMenuChoice(int userChoice, int menu) {
+			switch (menu) {
+				case 1:
+					return (userChoice > 0 && userChoice <= 3) ? true : false;
+				case 2:
+					return (userChoice > 0 && userChoice <= 6) ? true : false;
+				case 3:
+					return (userChoice > 0 && userChoice <= 5) ? true : false;
+				case 4:
+					return (userChoice > 0 && userChoice <= 1) ? true : false;
+				default:
+					return false;
+			}
+		}
 
-      public static void MainMenu(int menuChoice, ref int menuOption,
-         ref bool exitProgram) {
-         switch (menuChoice) {
-            case 1:
-               menuOption = 2;
-               break;
-            case 2:
-               menuOption = 3;
-               break;
-            case 3:
-               exitProgram = true;
-               Console.WriteLine("User has exited program.");
-               break;
-         }
-      }
+		public static void MainMenu(int menuChoice, ref int menuOption,
+			ref bool exitProgram) {
+			switch (menuChoice) {
+				case 1:
+					menuOption = 2;
+					break;
+				case 2:
+					menuOption = 3;
+					break;
+				case 3:
+					exitProgram = true;
+					Console.WriteLine("User has exited program.");
+					break;
+			}
+		}
 
-      public static void StandardMenu(int menuChoice, ref int menuOptions) {
-         switch (menuChoice) {
-            case 1:
-               DisplayStandards();
-               break;
-            case 2:
-               businessL.AddStandard(CreateStandard());
-               break;
-            case 3:
-               break;
-            case 4:
-               businessL.RemoveStandard(DeleteStandard());
-               break;
-            case 5:
-               break;
-            case 6:
-               menuOptions = 1;
-               Console.WriteLine("Returning to the Main Menu.");
-               break;
-         }
+		public static void StandardMenu(int menuChoice, ref int menuOptions) {
+			switch (menuChoice) {
+				case 1:
+					DisplayStandards();
+					break;
+				case 2:
+					businessL.AddStandard(CreateStandard());
+					break;
+				case 3:
+					menuOptions = 4;
+					break;
+				case 4:
+					businessL.RemoveStandard(DeleteStandard());
+					break;
+				case 5:
+					break;
+				case 6:
+					menuOptions = 1;
+					Console.WriteLine("Returning to the Main Menu.");
+					break;
+			}
+		}
 
-      }
+		public static void StudentSubMenu(int menuChoice, ref int menuOptions) {
+			switch (menuChoice) {
+				case 1:
+					UpdateStudent(1);
+					break;
+				case 2:
+					UpdateStudent(2);
+					break;
+			}
+			menuOptions = 3;
+		}
 
-      public static void StudentMenu(int menuChoice, ref int menuOptions) {
-         switch (menuChoice) {
-            case 1:
-               DisplayStudents();
-               break;
-            case 2:
-               businessL.AddStudent(CreateStudent());
-               break;
-            case 3:
-               break;
-            case 4:
-               businessL.RemoveStudent(DeleteStudent());
-               break;
-            case 5:
-               menuOptions = 1;
-               Console.WriteLine("Returning to Main Menu.");
-               break;
-         }
-      }
+		public static void StudentMenu(int menuChoice, ref int menuOptions) {
+			switch (menuChoice) {
+				case 1:
+					DisplayStudents();
+					break;
+				case 2:
+					businessL.AddStudent(CreateStudent());
+					break;
+				case 3:
+					menuOptions = 4;
+					break;
+				case 4:
+					businessL.RemoveStudent(DeleteStudent());
+					break;
+				case 5:
+					menuOptions = 1;
+					Console.WriteLine("Returning to Main Menu.");
+					break;
+			}
+		}
 
-      public static void DisplayStandards() {
-         Console.WriteLine("\nCurrent Standards:");
-         foreach (var element in businessL.GetAllStandards()) {
-            Console.WriteLine(element.StandardId + ": " 
-               + element.StandardName);
-         }
-         Console.WriteLine();
-      }
+		public static void DisplayStandards() {
+			Console.WriteLine("\nCurrent Standards:");
+			foreach (var element in businessL.GetAllStandards()) {
+				Console.WriteLine(element.StandardId + ": "
+					+ element.StandardName);
+			}
+			Console.WriteLine();
+		}
 
-      public static void DisplayStudents() {
-         Console.WriteLine("\nCurrent Students:");
-         foreach (var element in businessL.GetAllStudents()) {
-            Console.WriteLine(element.StudentID + ": " + element.StudentName);
-         }
-         Console.WriteLine();
-      }
+		public static void DisplayStudents() {
+			Console.WriteLine("\nCurrent Students:");
+			foreach (var element in businessL.GetAllStudents()) {
+				Console.WriteLine(element.StudentID + ": " + element.StudentName);
+			}
+			Console.WriteLine();
+		}
 
-      public static DataAccessLayer.Standard CreateStandard() {
-         Console.Write("Enter a name: ");
-         var name = Console.ReadLine();
+		public static DataAccessLayer.Standard CreateStandard() {
+			Console.Write("Enter a name: ");
+			var name = Console.ReadLine();
 
-         Console.Write("Enter a Standard Description: ");
-         var standardDesc = Console.ReadLine();
+			Console.Write("Enter a Standard Description: ");
+			var standardDesc = Console.ReadLine();
 
-         return new DataAccessLayer.Standard() {
-            StandardName = name,
-            Description = standardDesc
-         };
-      }
+			return new DataAccessLayer.Standard() {
+				StandardName = name,
+				Description = standardDesc
+			};
+		}
 
-      public static DataAccessLayer.Student CreateStudent() {
-         Console.Write("Enter a name: ");
-         var name = Console.ReadLine();
+		public static DataAccessLayer.Student CreateStudent() {
+			Console.Write("Enter a name: ");
+			var name = Console.ReadLine();
 
-         Console.Write("Enter a StandardId: ");
-         var standardId = Int32.Parse(Console.ReadLine());
+			Console.Write("Enter a StandardId: ");
+			var standardId = Int32.Parse(Console.ReadLine());
 
-         return new DataAccessLayer.Student() {
-            StudentName = name,
-            StandardId = standardId
-         };
-      }
+			return new DataAccessLayer.Student() {
+				StudentName = name,
+				StandardId = standardId
+			};
+		}
 
-      public static DataAccessLayer.Standard DeleteStandard() {
-         Console.Write("Enter the StandardID of the standard to delete: ");
-         return businessL.GetStandardByID(Int32.Parse(Console.ReadLine()));
-      }
+		public static DataAccessLayer.Standard DeleteStandard() {
+			Console.Write("Enter the StandardID of the standard to delete: ");
+			return businessL.GetStandardByID(Int32.Parse(Console.ReadLine()));
+		}
 
-      public static DataAccessLayer.Student DeleteStudent() {
-         Console.Write("Enter the StudentID of the student to delete: ");
-         return businessL.GetStudentByID(Int32.Parse(Console.ReadLine()));
-      }
+		public static DataAccessLayer.Student DeleteStudent() {
+			Console.Write("Enter the StudentID of the student to delete: ");
+			return businessL.GetStudentByID(Int32.Parse(Console.ReadLine()));
+		}
 
-      /* static DataAccessLayer.Student UpdateStudent() {
+		static DataAccessLayer.Student UpdateStudent(int searchChoice) {
+			DataAccessLayer.Student student;
+			DisplayStudents();
+			if (searchChoice == 1) {
+				do {
+					Console.Write("Enter the StudentID of the student to update: ");
+					student = businessL.GetStudentByID(Int32.Parse(Console.ReadLine()));
+				} while (student == null);
+			}
+			else {
+				do {
+					Console.Write("Enter the name of the Student to update: ");
+					student = businessL.GetStudentByName(Console.ReadLine());
+				} while (student == null);
+			}
+
+			Console.Write("Enter the new name (empty if not updating): ");
+			var name = Console.ReadLine();
+
+			Console.Write("Enter the new StandardID (empty if not updating): ");
+			var standardId = Console.ReadLine();
+
+			student.StudentName = !name.Equals("") ? name : student.StudentName;
+			student.StandardId = !standardId.Equals("") ? Int32.Parse(standardId) : student.StandardId;
+
+			return student;
+		}
+
+		/* static DataAccessLayer.Student UpdateStudent() {
            Console.Write("Enter the StudentID of the student to update: ");
            var studentId = Int32.Parse(Console.ReadLine());
 
@@ -260,5 +313,5 @@ namespace BusinessLayer {
            var name = Console.ReadLine();
        }*/
 
-   }
+	}
 }
